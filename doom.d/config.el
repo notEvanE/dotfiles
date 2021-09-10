957189e93b277bd78043aa0dd488e55452481ca4
Evan Erickson <90284041+notEvanE@users.noreply.github.com>
2021-09-09 18:08:16 -0600

    Added Doom Emacs config directory.

---
+++ doom.d/config.el
@@ -0,0 +1,95 @@
+;; org-babel-load-file
+
+(setq user-full-name "Evan Erickson"
+      user-mail-address "evan.erksn@gmail.com")
+
+(setq doom-theme 'doom-xcode)
+(custom-theme-set-faces! 'doom-xcode
+  ;;'(default :background "#1C1C1C")
+  '(default :background "#151515")
+  ;;'(default :background "#000000")
+  )
+
+(custom-theme-set-faces! 'doom-palenight
+  ;;'(default :background "#1C1C1C")
+  '(default :background "#151515")
+  ;;'(default :background "#000000")
+  )
+
+(custom-theme-set-faces! 'doom-xcode
+  ;;'(default :background "#1C1C1C")
+  '(default :background "#151515")
+  ;;'(default :background "#000000")
+  )
+
+(setq doom-font (font-spec :family "Dank Mono" :size 14)
+      doom-big-font (font-spec :family "Dank Mono" :size 24))
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+(setq display-line-numbers-type t)
+(map! :leader
+      (:prefix ("t" . "toggle")
+       :desc "Comment or uncomment lines" "/" #'comment-line
+       :desc "Toggle line numbers" "l" #'doom/toggle-line-numbers
+       :desc "Toggle line highlight in frame" "h" #'hl-line-mode
+       :desc "Toggle line highlight globally" "H" #'global-hl-line-mode
+       :desc "Toggle truncate lines" "t" #'toggle-truncate-lines))
+
+(use-package! ace-window
+  :ensure t
+  :config
+    (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+
+(use-package! key-chord
+  :ensure t
+  :config
+    (key-chord-mode 1)
+    (setq key-chord-two-keys-delay 0.05)
+    (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+    (key-chord-define-global "vv" 'ace-window))
+
+;;(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(setq initial-frame-alist
+      '(
+        ;;(top . 1)
+        ;;(left . 1)
+        (width . 143)
+        (height . 55)))
+
+(after! org
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (setq org-directory "~/Org/"
+        org-agenda-files '("~/Org/agenda.org")
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-ellipsis " ▼ "
+        org-log-done 'time
+        org-journal-dir "~/Org/journal/"
+        org-journal-date-format "%B %d, %Y (%A) "
+        org-journal-file-format "%Y-%m-%d.org"
+        org-hide-emphasis-markers t
+        ;; ex. of org-link-abbrev-alist in action
+        ;; [[arch-wiki:Name_of_Page][Description]]
+        org-link-abbrev-alist    ; This overwrites the default Doom org-link-abbrev-list
+          '(("google" . "http://www.google.com/search?q=")
+            ("arch-wiki" . "https://wiki.archlinux.org/index.php/")
+            ("ddg" . "https://duckduckgo.com/?q=")
+            ("wiki" . "https://en.wikipedia.org/wiki/"))
+        org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
+          '((sequence
+             "TODO(t)"           ; A task that is ready to be tackled
+             "BLOG(b)"           ; Blog writing assignments
+             "GYM(g)"            ; Things to accomplish at the gym
+             "PROJ(p)"           ; A project that contains other tasks
+             "VIDEO(v)"          ; Video assignments
+             "WAIT(w)"           ; Something is holding up this task
+             "|"                 ; The pipe necessary to separate "active" states and "inactive" states
+             "DONE(d)"           ; Task has been completed
+             "CANCELLED(c)" )))) ; Task has been cancelled
+
+(map! :leader
+      (:desc "Open in Finder" "z" #'reveal-in-osx-finder))
---
+++ doom.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-safe-themes
+   '("1f1b545575c81b967879a5dddc878783e6ebcca764e4916a270f9474215289e5" "a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" default))
+ '(package-selected-packages
+   '(spacemacs-theme json-mode ediprolog cyberpunk-theme cyberpunk-2019-theme cobol-mode auctex ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
